aba32946a98493652fcd48d64ee416299b9a6619
ShellyHZX <shellylove1119@gmail.com>
2026-04-24 01:40:47 +1000

    Revert "add article CRUD feature"
    This reverts commit aa03ec564c9015a49770117de00f12e67d60e4b0.
    
    :wq

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.routers import auth, admin, article
+from app.routers import auth, admin
 from app.database import connect_db, close_db
 from app.models.user import User
 from app.models.counter import Counter
-from app.models.article import Article
 from app.config import settings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await connect_db(document_models=[User, Counter, Article])
+    await connect_db(document_models=[User, Counter])
     yield
     await close_db()
 
@@ -19,4 +18,3 @@ app = FastAPI(title="IP Group Assignment API", lifespan=lifespan, root_path=sett
 
 app.include_router(auth.router)
 app.include_router(admin.router)
-app.include_router(article.router)
